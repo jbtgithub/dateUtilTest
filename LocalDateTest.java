f97d620bcf72c5d266758b36e0fad32ba8ef87c6
jbtgithub <jungbt82@gmail.com>
2016-12-23 18:05:57 +0900

    Add files via upload

---
+++ LocalDateTest.java
@@ -0,0 +1,12 @@
+package testJavaf;
+
+import java.time.LocalDate;
+
+public class LocalDateTest {
+	public static void main(String[] args) {
+		LocalDate today = LocalDate.now();
+		LocalDate tomorrow = today.plusDays(1);
+		
+		System.out.println("today="+today+", tomorrow="+tomorrow);
+	}
+}
